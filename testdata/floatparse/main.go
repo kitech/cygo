936fbf4b32f7982eba6433e8be34dfba19942271
Peter Waller <p@pwaller.net>
2019-02-10 22:55:45 +0000

    Make floatparse slightly more interesting

--- testdata/floatparse/main.go
+++ testdata/floatparse/main.go
@@ -7,6 +7,8 @@ func main() {
 	for i := 0; i < 200000000; i++ {
 		x, _ := strconv.ParseFloat("12.34567", 64)
 		y += x
+		z, _ := strconv.ParseFloat("2.34567", 64)
+		y += z
 	}
 
 	println("hello world", int(y))
