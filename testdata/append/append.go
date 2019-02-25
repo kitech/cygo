afa707392fb8f3e034137f39b513736b90faded7
Peter Waller <p@pwaller.net>
2019-02-25 13:21:58 +0000

    Tidy: improve slicing tests

--- testdata/append/append.go
+++ testdata/append/append.go
@@ -2,7 +2,21 @@ package main
 
 func main() {
 	var x []int
+	println(x)
 	x = append(x, 1)
-	x = append(x, 1, 2, 3, 4)
-	println(x[0], x[1], x[len(x)-1])
+	println(x, x[0])
+	x = append(x, 2)
+	println(x, x[0], x[1])
+	x = append(x, 123456)
+	println(x, x[0], x[1], x[3])
+
+	x = append(x, 99, 98, 87)
+
+	dumpSlice(x)
+}
+
+func dumpSlice(x []int) {
+	for i, v := range x {
+		println(i, v)
+	}
 }
--- testdata/slicing/slicing.go
+++ testdata/slicing/slicing.go
@@ -4,6 +4,7 @@ func main() {
 	bytes()
 	strings()
 	structs()
+	arrays()
 }
 
 func bytes() {
@@ -23,4 +24,12 @@ func structs() {
 	type foo struct{ x int }
 	s := []foo{{1}, {2}}
 	println(s[1:][0].x)
+}
+
+func arrays() {
+	x := [...]int{1, 2, 3, 4}
+	println(x[:][0], x[:][1], x[:][2], x[:][3])
+	println(x[1:][0], x[1:][1], x[1:][2])
+	println(x[1:3][0], x[1:3][1])
+	println(x[:2][0], x[:2][1])
 }
