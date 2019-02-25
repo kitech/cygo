f724472469a18bb12e1b9e6c0dce646cd37fd89e
Peter Waller <p@pwaller.net>
2019-02-25 15:42:28 +0000

    Add structs test to append.go

--- testdata/append/append.go
+++ testdata/append/append.go
@@ -1,6 +1,11 @@
 package main
 
 func main() {
+	ints()
+	structs()
+}
+
+func ints() {
 	var x []int
 	println(x)
 	x = append(x, 1)
@@ -8,14 +13,25 @@ func main() {
 	x = append(x, 2)
 	println(x, x[0], x[1])
 	x = append(x, 123456)
-	println(x, x[0], x[1], x[3])
+	println(x, x[0], x[1], x[2])
 
 	x = append(x, 99, 98, 87)
 
-	dumpSlice(x)
+	intsDumpSlice(x)
 }
 
-func dumpSlice(x []int) {
+func structs() {
+	type foo struct {
+		x, y int
+	}
+	var s []foo
+	s = append(s, foo{1, 2})
+	s = append(s, foo{3, 4})
+	println(s[0].x, s[0].y)
+	println(s[1].x, s[1].y)
+}
+
+func intsDumpSlice(x []int) {
 	for i, v := range x {
 		println(i, v)
 	}
