d9d5d7ed3984d7a8d9c9e3d0269e5790dc433651
egitop <egitop@uload.xyz>
2019-05-16 18:35:12 +0800

    clean

--- noro/noro.h
+++ noro/noro.h
@@ -3,5 +3,15 @@
 #define _NORO_H_
 
 
+typedef struct noro noro;
+
+noro* noro_get();
+noro* noro_new();
+void noro_init(noro* nr);
+void noro_free(noro* lnr);
+void noro_wait_init_done(noro* nr);
+
+void noro_post(void(*fn)(void*arg), void*arg);
+
 
 #endif
