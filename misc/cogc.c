1bd8e28d3a1eb4f8570c67e42febe7e7afb852aa
egitop <egitop@uload.xyz>
2019-05-21 14:55:30 +0800

    fix cogc0, work fine

--- misc/cogc.c
+++ misc/cogc.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+#include <pthread.h>
+#include <unistd.h>
 #include <ucontext.h>
 #include <gc.h>
 
@@ -7,18 +11,37 @@
 
 ucontext_t child, parent;
 
+struct thr_hndl_sb_s {
+    void *gc_thread_handle;
+    struct GC_stack_base sb;
+    void *bottom;
+};
+struct thr_hndl_sb_s sb0 = {0};
+struct thr_hndl_sb_s sb1 = {0};
+struct thr_hndl_sb_s sb2 = {0};
+
 void threadFunction() {
+    GC_set_stackbottom123((void*)pthread_self(), sb1.bottom); // this works
     GC_gcollect();
+    GC_MALLOC(700);
 
     printf("Child: Switch to parent\n");
     swapcontext( &child, &parent );
+    GC_set_stackbottom123((void*)pthread_self(), sb1.bottom); // this works
+    GC_gcollect();
     printf("Child: Switch to parent\n");
     swapcontext( &child, &parent );
 }
 
+extern char* GC_stackbottom;
 int main() {
     GC_init();
+    sb0.gc_thread_handle = GC_get_my_stackbottom(&sb0.sb);
+    sb0.bottom = GC_stackbottom;
+    assert(GC_stackbottom == sb0.sb.mem_base);
+
     void *mem = GC_MALLOC(1000);
+
 
     getcontext( &child );
     child.uc_link = 0;
@@ -29,14 +52,20 @@ int main() {
         perror( "malloc: Could not allocate stack" );
         exit( 1 );
     }
+    sb1.sb.mem_base = child.uc_stack.ss_sp;
+    sb1.bottom = (void*)((uintptr_t)(sb1.sb.mem_base) + STACK_SIZE);
     makecontext( &child, &threadFunction, 0);
 
     printf("Parent: Switch to child\n");
     swapcontext( &parent, &child );
+    GC_set_stackbottom123((void*)pthread_self(), sb0.sb.mem_base);
     printf("Parent: Switch to child\n");
     swapcontext( &parent, &child );
+    GC_set_stackbottom123((void*)pthread_self(), sb0.sb.mem_base);
 
     GC_free( child.uc_stack.ss_sp );
+    GC_gcollect();
+    sleep(3);
     return 0;
 }
 
