2e7daf0dae73aa3d2960412da5b34b9c49fc04dd
egitop <egitop@uload.xyz>
2019-06-16 23:51:51 +0800

    add pubcrn script

---
+++ scripts/pubcrn.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+rm -rf ./crnclr
+git clone . crnclr
+du -hs crnclr
+
+cd crnclr
+
+skip_commit()
+{
+	  shift;
+	  while [ -n "$1" ];
+	  do
+		    shift;
+		    map "$1";
+		    shift;
+	  done;
+}
+
+# delete imported subtree
+git filter-branch -f --commit-filter '
+	if [ "$GIT_AUTHOR_EMAIL" = "p@pwaller.net" ];
+	then
+		skip_commit "$@";
+	else
+		git commit-tree "$@";
+	fi' HEAD
+
+# delete un
+git filter-branch -f --commit-filter '
+  has0=$(git show $GIT_COMMIT --name-status | egrep ^[AMD] | grep noro)
+  has1=$(git show $GIT_COMMIT --name-status | egrep ^[AMD] | grep corona)
+  has2=$(git show $GIT_COMMIT --name-status | egrep ^[AMD] | grep readme.md)
+  has3=$(git show $GIT_COMMIT --name-status | egrep ^[AMD] | grep docs/debug.md)
+  if [ "$has0" != "" ] || [ "$has1" != "" ] || [ "$has2" != "" ] || [ "$has3" != "" ]; then
+      git commit-tree "$@";
+  else
+      skip_commit "$@";
+  fi
+	' HEAD
+
+git filter-branch -f --tree-filter "rm -fr byir bysrc include misc scripts docs/pass.md Makefile" HEAD
+
+git gc
+git log --oneline | wc -l
+
+cd -
+du -hs crnclr
