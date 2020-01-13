a0d38d4f51abcdcca78639aca406f868fea93f19
drswinghead <drswinghead@gmail.com>
2020-01-13 13:52:06 +0800

    anltr go target so slow?

--- golang/main1.go
+++ golang/main1.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"time"
 
 	"github.com/antlr/antlr4/runtime/Go/antlr"
 
@@ -9,15 +11,42 @@ import (
 )
 
 func main() {
-	fstm, err := antlr.NewFileStream("./examples/function.go")
+	fstm, err := antlr.NewFileStream("./examples/method.go")
+	if err != nil {
+		log.Panicln(err, fstm)
+	}
+	bcc, err := ioutil.ReadFile("./examples/method.go")
 	if err != nil {
 		log.Panicln(err)
 	}
-	lexer := parser.NewGoLexer(fstm)
-	tkstm := antlr.NewCommonTokenStream(lexer, 0)
+
+	strstm := antlr.NewInputStream(string(bcc))
+	// lexer := parser.NewGoLexer(fstm)
+	lexer := parser.NewGoLexer(strstm) // still slow
+	tkstm := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
 
 	// Create the Lexer
 	prs := parser.NewGoParser(tkstm)
+	log.Println("prs", prs)
 
-	log.Println(prs)
+	btime := time.Now()
+	antlr.ParseTreeWalkerDefault.Walk(&dumpgoListerner{}, prs.SourceFile())
+	dtime := time.Since(btime)
+	log.Println(dtime) // about 1.2s, so slow for donothing?
+}
+
+type dumpgoListerner struct {
+	parser.BaseGoParserListener
+}
+
+func (l *dumpgoListerner) EnterSourceFile(c *parser.SourceFileContext) {
+	log.Println(c.AllDeclaration())
+}
+func (l *dumpgoListerner) EnterImportDecl(c *parser.ImportDeclContext) {
+	log.Println(c.GetText())
+	log.Println(c.AllImportSpec())
+}
+
+func (l *dumpgoListerner) EnterMethodDecl(c *parser.MethodDeclContext) {
+	log.Println(c.Receiver().GetChildCount())
 }
