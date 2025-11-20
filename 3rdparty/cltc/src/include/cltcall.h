7a69d9239e83b9392e008b79a96b233b1d215e23
egitop <egitop@uroad.xyz>
2025-11-20 11:06:38 +0800

    mrghdr

---
+++ 3rdparty/cltc/src/include/cltcall.h
@@ -0,0 +1,10 @@
+#ifndef _CLTC_ALL_H_
+#define _CLTC_ALL_H_
+
+#include "common.h"
+#include "array.h"
+#include "deque.h"
+#include "queue.h"
+#include "hashtable.h"
+
+#endif // _CLTC_ALL_H_
---
+++ docs/resources.md
@@ -0,0 +1,6 @@
+### urls
+* https://c-util.github.io/
+* https://github.com/nemequ/portable-snippets
+
+### search keywords:
+* collect c github
